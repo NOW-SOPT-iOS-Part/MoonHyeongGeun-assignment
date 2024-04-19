d216ffdc91df8079f673936c9b952e90a68ddcc7
문형근 <81503607+Chandrarla@users.noreply.github.com>
2024-04-19 14:14:07 +0900

    [config] 프로젝트 초기 설정

---
+++ 34th-Sopt-Assignment/34th-Sopt-Assignment/ViewController/WelcomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  34th-Sopt-Assignment
+//
+//  Created by Chandrala on 4/19/24.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

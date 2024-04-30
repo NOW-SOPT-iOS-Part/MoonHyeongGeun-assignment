c11ee0991d6090681076301ed61a3e6eef3dfc3f
문형근 <81503607+Chandrarla@users.noreply.github.com>
2024-04-30 17:41:46 +0900

    [Chore] 폴더명 변경

---
+++ 34th-Sopt-Assignment/34th-Sopt-Assignment/Presentation/Home/Controllers/HomeViewController.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+final class HomeViewController: UIViewController {
+    // MARK: - Component
+    private let topTabbar = UICollectionView()
+    private let topCollectionView = UICollectionView()
+    private let mustWatchCollectionView = UICollectionView()
+    private let liveCollectionView = UICollectionView()
+    private let paramountCollectionView = UICollectionView()
+    private let magicMovieCollectionView = UICollectionView()
+    
+    // MARK: - LifeCycle
+    override func viewDidLoad() {
+    }
+    
+    // MARK: - SetUI
+    
+    // MARK: - Action
+    
+    // MARK: - AutoLayout
+}

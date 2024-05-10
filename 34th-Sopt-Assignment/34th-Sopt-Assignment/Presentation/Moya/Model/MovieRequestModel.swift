a8e3ed97f87d1aa5635aec6331e7a24f55804387
문형근 <81503607+Chandrarla@users.noreply.github.com>
2024-05-10 23:42:28 +0900

    [feat] Moya를 사용하여 GET 메소드 구현

---
+++ 34th-Sopt-Assignment/34th-Sopt-Assignment/Presentation/Moya/Model/MovieRequestModel.swift
@@ -0,0 +1,14 @@
+//
+//  SignUpRequestModel.swift
+//  34th-Sopt-Assignment
+//
+//  Created by Chandrala on 5/10/24.
+//
+
+import Foundation
+
+struct MovieRequestModel: Codable {
+    let key: String
+    let targetDT: String
+    let itemPerPage: String
+}
